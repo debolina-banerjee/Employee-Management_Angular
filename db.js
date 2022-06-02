40c48541f454c10d4bf023dc05b8cb9dd39d402e
Debolina Banerjee <102504798+debolina-banerjee@users.noreply.github.com>
2022-06-02 16:43:58 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url=`mongodb+srv://Lina_B:turtle111@cluster0.udwzo.mongodb.net/?retryWrites=true&w=majority`
+const url=`mongodb+srv://user_name:password@cluster0.udwzo.mongodb.net/?retryWrites=true&w=majority`
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
